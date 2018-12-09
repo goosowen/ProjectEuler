4ea1d86b8c5a23930286d6150c7329c7a912b42e
Alex Markovits <akmarkov@mit.edu>
2018-12-08 16:32:39 -0800

    problem 19

--- solvers/euler019.py
+++ solvers/euler019.py
@@ -24,7 +24,45 @@ century (1 Jan 1901 to 31 Dec 2000)?
 
 
 def main():
-    return "unimplemented"
+    days_in_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    sunday = 7
+    months_in_year = [('Jan',31), ('Feb',28), ('Mar',31), ('Apr',30), ('May',31), ('Jun',30), ('Jul',31), ('Aug',31),
+              ('Sep',30), ('Oct',31), ('Nov',30), ('Dec',31)]
+
+    year = 1900
+    weekday = 1
+    month = 1
+    days_in_month = months_in_year[month - 1][1]
+    month_day = 1
+
+    count = 0
+    tot = 0
+    while year < 2001 and count < 100000:
+        if year > 1900 and month_day == 1 and weekday == sunday:
+            tot += 1
+
+        weekday += 1
+        if weekday > len(days_in_week):
+            weekday = 1
+
+        month_day += 1
+        if month_day > days_in_month:
+            month_day = 1
+
+            month += 1
+            if month > len(months_in_year):
+                month = 1
+                year += 1
+
+            days_in_month = months_in_year[month - 1][1]
+            if month == 2 and year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                days_in_month += 1
+
+        count += 1
+
+        # if count % 5 == 0:
+        #     print(days_in_week[weekday-1], months_in_year[month-1][0], month_day, year)
+    return tot
 
 
 if __name__ == "__main__":
