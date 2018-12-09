532a67d140581dca5b936773b767b8520ff5ab58
Alex Markovits <akmarkov@mit.edu>
2018-12-08 19:04:04 -0800

    working on 50

--- solvers/euler050.py
+++ solvers/euler050.py
@@ -20,8 +20,50 @@ consecutive primes?
 """
 
 
+# TODO lazy initialization of primes for runner
+# TODO this is algorithmically way too slow. Need a better subproblem/approach.
 def main():
-    return "unimplemented"
+    from common.euler_functions import is_prime_mem
+
+    max_consecutive = 0
+    best_consecutive = []
+    best_num = 0
+
+    limit = 10**6
+
+    primes = [2]
+    for i in range(3, limit, 2):
+        if is_prime_mem(i, primes):
+            primes.append(i)
+
+    print(len(primes))
+
+    # def get_consecutives(i, j, consecutives, primes):
+    #     if i == j:
+    #         return primes[i]
+    #
+    #     return get_consecutives()
+
+    consecutives = {}
+    for i in range(len(primes)):
+        for j in range(i, len(primes)):
+            if i == j:
+                consecutives[(i, j)] = primes[j]
+            else:
+                consecutives[(i, j)] = consecutives[(i, j-1)] + primes[j]
+
+            if consecutives[(i, j)] in primes and j-i+1 > max_consecutive:
+                max_consecutive = j-i+1
+                best_consecutive = primes[i:j+1]
+                best_num = consecutives[(i, j)]
+                print(best_num, max_consecutive, best_consecutive)
+                assert best_num == sum(best_consecutive)
+
+            if consecutives[(i, j)] > limit:
+                break
+
+    print(best_num, max_consecutive, best_consecutive)
+    return best_num
 
 
 if __name__ == "__main__":
